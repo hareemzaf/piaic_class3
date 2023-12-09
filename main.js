ef12d411f07481db47026d210878c43066d9895d
hareemzaf <150966998+hareemzaf@users.noreply.github.com>
2023-12-09 19:56:57 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,79 @@
+// let username = "hif";
+// console.log("hif");
+// 1st example
+var studentGender = "female";
+if (studentGender === "male") {
+    console.log("entry left side");
+}
+else {
+    console.log("entry right side");
+}
+// 2nd example
+var studentGrade = 90;
+if (studentGrade === 90) {
+    console.log("you  got A grade");
+}
+else {
+    console.log("you  got A grade");
+}
+// 3rd example
+var salary = 50000;
+var monthlyexpence = 80000;
+if (salary === 40000) {
+    console.log("you have not enough money");
+}
+else {
+    console.log("you have enough money");
+}
+// 4th example
+if (2 > 3) {
+    console.log("2 grater than 3");
+}
+// 5th example
+console.log("taking decision");
+var raita = true;
+var biscuit = true;
+if (raita) {
+    console.log("buy samosa");
+}
+else if (biscuit) {
+    console.log("buy biscuit");
+}
+else {
+    console.log("don't buy");
+}
+// 6th example
+console.log("taking decision");
+var buget = 40000;
+var cycle = 40000;
+var color = "blue";
+// if(((cycle <= buget && || color=="red" ) (color == "black" || color=="blue")) )
+if (((color == "black" || color == "blue" || color == "red") && cycle <= buget)) {
+    // if(color=="black"){
+    //     console.log("buy cycle")
+    // } else{
+    //     console.log("color is not match")
+    // }
+    console.log("buy cycle");
+}
+else {
+    console.log("i need to bulid my skils to eran more");
+}
+// single =
+var num = "2"; // js ma any ka koi concept nhi h
+var lastname = "hif";
+console.log(num); //2
+// double == comprision match just vaue match
+console.log(num == 2 || lastname == "hif"); //true
+// triple === match do ki data type bhi match kera ga
+console.log(num === 2); //false
+//  single assign
+var car = "BMW";
+var carcolor = "red";
+if (carcolor != "black") {
+    console.log("color is not red");
+}
+// =
+// == !=
+// === !==
+console.log("43");
---
+++ main.ts
@@ -0,0 +1,105 @@
+// // let username = "hif";
+    
+// // console.log("hif");
+
+// // 1st example
+
+// let studentGender:any = "female";
+// if(studentGender === "male") {
+//     console.log("entry left side")
+// }
+// else{
+//     console.log("entry right side")
+// }
+
+// // 2nd example
+
+// let studentGrade:any = 90;
+// if(studentGrade === 90) {
+//     console.log("you  got A grade")
+// }
+// else{
+//     console.log("you  got A grade")
+// }
+
+// // 3rd example
+
+// let salary=50000
+// let monthlyexpence = 80000
+// if(salary===40000){
+//     console.log("you have not enough money")
+// } else {
+//     console.log("you have enough money")
+// }
+
+
+
+// // 4th example
+
+// if(2>3){
+//     console.log("2 grater than 3")
+// }
+
+// // 5th example
+ 
+// console.log("taking decision")
+// let raita=true
+// let biscuit=true
+
+// if(raita) {
+//     console.log("buy samosa")
+// }
+// else if(biscuit){
+//     console.log("buy biscuit")
+// }
+// else{
+//     console.log("don't buy")
+// }
+// // 6th example
+
+// console.log("taking decision");
+// let buget = 40000
+// let cycle = 40000
+// let color="blue"
+// // if(((cycle <= buget && || color=="red" ) (color == "black" || color=="blue")) )
+// if(((color == "black" || color=="blue" || color=="red") && cycle <= buget))
+// {
+//     // if(color=="black"){
+//     //     console.log("buy cycle")
+//     // } else{
+//     //     console.log("color is not match")
+
+//     // }
+//     console.log("buy cycle")
+// }
+// else{
+//     console.log("i need to bulid my skils to eran more")
+// } 
+
+// // single =
+// let num:any ="2"; // js ma any ka koi concept nhi h
+// let lastname = "hif"
+// console.log(num) //2
+// // double == comprision match just vaue match
+// console.log(num == 2 || lastname=="hif" ) //true
+
+// // triple === match do ki data type bhi match kera ga
+
+// console.log(num === 2) //false
+
+// //  single assign
+// let car:string="BMW";
+// let carcolor:string="red";
+// if(carcolor!="black"){
+//     console.log("color is not red")
+// }
+
+
+
+
+
+
+
+
+
+
